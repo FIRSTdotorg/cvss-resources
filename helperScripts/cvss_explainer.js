75f17fb9f57c57e177e0628720929f11f1c2b1b7
Nick Leali <nleali@cisco.com>
2024-09-27 14:44:11 -0400

    v4 formatting and updates
    More formatting for the description and expanding some of the details.

--- helperScripts/cvss_explainer.js
+++ helperScripts/cvss_explainer.js
@@ -18,9 +18,10 @@ const EXPLANATIONS = {
             AC: {
                 L: "No built-in security-enhancing conditions exist within the product to inhibit successful exploitation.",
                 H: "The attacker must defeat security protections to exploit the vulnerability."
+            },
             AT: {
-                L: "No attack requirements are present.",
-                H: "Attackers must prepare the environment prior to an attack."
+                N: "No attack requirements are present.",
+                P: "Attackers must prepare the environment prior to an attack."
             },
             PR: {
                 N: "No privileges are required for an attacker to successfully exploit the vulnerability.",
@@ -46,7 +47,7 @@ const EXPLANATIONS = {
                 H: "The vulnerable system may become unavailable or unusable for a long time.",
                 L: "The vulnerable system may experience some temporary disruptions.",
                 N: "The system's availability is not affected by this attack."
-            }
+            },
             SC: {
                 H: "Sensitive data could be highly compromised on subsequent systems.",
                 L: "Some data could be compromised on subsequent systems, but the impact is limited.",
@@ -80,7 +81,7 @@ if (process.argv.length < 3) {
 const cvssVector = process.argv[2];
 
 // Basic format validation for the CVSS vector (CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:H/VI:N/VA:N/SC:N/SI:N/SA:N)
-const cvssRegex = /^CVSS:4\.0\/AV:[NALP]\/AC:[LH]\/AT:[LH]\/PR:[NLH]\/UI:[NPA]\/VC:[HLN]\/VI:[HLN]\/VA:[HLN]\/SC:[HLN]\/SI:[HLN]\/SA:[HLN]$/;
+const cvssRegex = /^CVSS:4\.0\/AV:[NALP]\/AC:[LH]\/AT:[NP]\/PR:[NLH]\/UI:[NPA]\/VC:[HLN]\/VI:[HLN]\/VA:[HLN]\/SC:[HLN]\/SI:[HLN]\/SA:[HLN]$/;
 
 if (!cvssRegex.test(cvssVector)) {
     console.log(EXPLANATIONS.en.errors.invalidFormat);
@@ -93,21 +94,27 @@ const metrics = cvssVector.split('/');
 // Extract the values of each metric
 const av = metrics[1].split(':')[1];
 const ac = metrics[2].split(':')[1];
-const pr = metrics[3].split(':')[1];
-const ui = metrics[4].split(':')[1];
-const s = metrics[5].split(':')[1];
-const c = metrics[6].split(':')[1];
-const i = metrics[7].split(':')[1];
-const a = metrics[8].split(':')[1];
+const at = metrics[3].split(':')[1];
+const pr = metrics[4].split(':')[1];
+const ui = metrics[5].split(':')[1];
+const vc = metrics[6].split(':')[1];
+const vi = metrics[7].split(':')[1];
+const va = metrics[8].split(':')[1];
+const sc = metrics[9].split(':')[1];
+const si = metrics[10].split(':')[1];
+const sa = metrics[11].split(':')[1];
 
 // Generate a narrative explanation for the CVSS vector
-let narrative = `This vulnerability can be described as follows: ${EXPLANATIONS.en.baseMetrics.AV[av]} `;
-narrative += `${EXPLANATIONS.en.baseMetrics.AC[ac]} `;
-narrative += `${EXPLANATIONS.en.baseMetrics.PR[pr]} `;
-narrative += `${EXPLANATIONS.en.baseMetrics.UI[ui]} `;
-narrative += `${EXPLANATIONS.en.baseMetrics.S[s]} `;
-narrative += `In terms of data protection, ${EXPLANATIONS.en.baseMetrics.C[c]} `;
-narrative += `Additionally, ${EXPLANATIONS.en.baseMetrics.I[i]} `;
-narrative += `Finally, from a system availability perspective, ${EXPLANATIONS.en.baseMetrics.A[a]}`;
+let narrative = `This vulnerability can be described as follows: \r\nExploitability Metrics \r\n\r\n ${EXPLANATIONS.en.baseMetricsv4.AV[av]} `;
+narrative += `${EXPLANATIONS.en.baseMetricsv4.AC[ac]} `;
+narrative += `${EXPLANATIONS.en.baseMetricsv4.AT[at]} `;
+narrative += `${EXPLANATIONS.en.baseMetricsv4.PR[pr]} `;
+narrative += `${EXPLANATIONS.en.baseMetricsv4.UI[ui]} \r\n`;
+narrative += `\r\nImpact Metrics\r\n\For the vulnerable system in terms of data protection, ${EXPLANATIONS.en.baseMetricsv4.VC[vc]} `;
+narrative += `Additionally, ${EXPLANATIONS.en.baseMetricsv4.VI[vi]} `;
+narrative += `Finally, from a system availability perspective, ${EXPLANATIONS.en.baseMetricsv4.VA[va]}`;
+narrative += `\r\nSubsequent systems may also be impacted. For downstream systems, ${EXPLANATIONS.en.baseMetricsv4.SC[sc]} `;
+narrative += `Additionally, ${EXPLANATIONS.en.baseMetricsv4.SI[si]} `;
+narrative += `Finally, from a system availability perspective, ${EXPLANATIONS.en.baseMetricsv4.SA[sa]}`;
 
 console.log(narrative);
